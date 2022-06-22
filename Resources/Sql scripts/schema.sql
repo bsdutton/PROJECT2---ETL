ea0a0ad62a62f56eba5ae9cae569a44af6600f30
Vasanta <vasanta75@gmail.com>
2022-06-21 17:37:39 -0700

    Two queries working

--- Resources/Sql scripts/schema.sql
+++ Resources/Sql scripts/schema.sql
@@ -1,7 +1,6 @@
--- Create tables for raw data to be loaded into
 Drop table movies CASCADE
 CREATE TABLE movies (
-id INT PRIMARY KEY,
+id BIGINT PRIMARY KEY,
 imdb_id VARCHAR,
 title TEXT,
 release_date DATE,
@@ -12,27 +11,31 @@ vote_average FLOAT
 Drop table genre
 CREATE TABLE genre(
 genreId INT PRIMARY KEY,	
-movie_id INT ,
+movie_id BIGINT ,
 genre TEXT,
-foreign key(movie_id) references movies(id) 
+foreign key(movie_id) references movies(id)
 );
 Drop table ratings
 CREATE TABLE ratings (
 userId INT ,
-movieId INT ,
+movieId BIGINT ,
 rating FLOAT,
-PRIMARY KEY(userId, movieId),
+PRIMARY KEY(movieId),
 foreign key(movieId) references movies(id)
 );
 Drop table links
 CREATE TABLE links (
 linkId INT PRIMARY KEY,
 imdbId VARCHAR,
-tmdbId INT,
+tmdbId BIGINT,
 foreign key(tmdbId) references movies(id)
 );
+select * from movies
+select * from genre
+select * from ratings
+select * from links
 
-select * from movies;
-select * from genre;
-select * from ratings;
-select * from links;
+select movies.title, ratings.rating
+from movies, ratings
+where movies.id = ratings.movieId
+order by rating desc limit 10
