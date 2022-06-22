3aaa3a7ada3c7a9f3e81062a209dc4572ae45f1c
Vasanta <vasanta75@gmail.com>
2022-06-21 19:06:33 -0700

    All queries working

--- Resources/Sql scripts/schema.sql
+++ Resources/Sql scripts/schema.sql
@@ -1,41 +1,21 @@
-Drop table movies CASCADE
-CREATE TABLE movies (
-id BIGINT PRIMARY KEY,
-imdb_id VARCHAR,
-title TEXT,
-release_date DATE,
-budget INT,
-revenue INT,
-vote_average FLOAT
-);
-Drop table genre
-CREATE TABLE genre(
-genreId INT PRIMARY KEY,	
-movie_id BIGINT ,
-genre TEXT,
-foreign key(movie_id) references movies(id)
-);
-Drop table ratings
-CREATE TABLE ratings (
-userId INT ,
-movieId BIGINT ,
-rating FLOAT,
-PRIMARY KEY(movieId),
-foreign key(movieId) references movies(id)
-);
-Drop table links
-CREATE TABLE links (
-linkId INT PRIMARY KEY,
-imdbId VARCHAR,
-tmdbId BIGINT,
-foreign key(tmdbId) references movies(id)
-);
-select * from movies
-select * from genre
-select * from ratings
-select * from links
+-- Table: public.ratings
 
+-- DROP TABLE IF EXISTS public.ratings;
+
+CREATE TABLE IF NOT EXISTS public.ratings
+(
+    "userId" bigint,
+    "movieId" bigint,
+    rating double precision
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.ratings
+    OWNER to postgres;
+	
+select * from ratings
 select movies.title, ratings.rating
 from movies, ratings
-where movies.id = ratings.movieId
-order by rating desc limit 10
+where movies.id in ratings.movieId
+order by ratings.rating desc limit 10
